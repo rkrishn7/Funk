8221786903b4d2e996ab53903e8bd661c3fdb60a
Rohan Krishnaswamy <rohan@rohankrishnaswamy.com>
2020-07-19 02:11:41 -0700

    setup

---
+++ src/components/input/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import { 
+  color, 
+  space, 
+  layout, 
+  ColorProps,
+  SpaceProps, 
+  LayoutProps 
+} from 'styled-system';
+import { Button } from '../button';
+
+interface InputProps
+  extends ColorProps,
+  SpaceProps,
+  LayoutProps {};
+
+const View = styled.div<InputProps>`
+  ${color}
+  ${layout}
+  ${space}
+  padding: 0.5em;
+  margin: 0.5em;
+  border: none;
+  border-radius: 3px;
+`;
+
+export const Boobee: React.FC<any> = (props) => {
+    return (
+        <View {...props}>
+            <Button>
+                {props.children}
+            </Button>
+        </View>
+    );
+};
---
+++ src/components/input/stories/input.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Boobee } from '../';
+
+export default { title: 'Boobee' };
+
+export const plainOne = () => <Boobee bg="black" color="white" >Hello</Boobee>
